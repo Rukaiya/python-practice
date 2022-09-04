af500a385491e170765e472bd40b9cef18eaab68
Rukaiya Amin <rukaiyaamin36@gmail.com>
2022-09-04 07:27:54 +0600

    enum

--- class/enumeration.py
+++ class/enumeration.py
@@ -20,4 +20,28 @@ try:
         print('The request completed successfully')
 except ValueError as error:
     print(error)
-    
+
+# customized enum class
+class PaymentStatus(Enum):
+    PENDING = 1
+    COMPLETED = 2
+    REFUNDED = 3
+
+    def __str__(self):
+        return f'{self.name.lower()}({self.value})'
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.value == other
+
+        if isinstance(other, PaymentStatus):
+            return self is other
+
+        return False
+
+
+if PaymentStatus.PENDING == PaymentStatus.PENDING:
+    print('The payment is pending.')
+
+if PaymentStatus.PENDING == 1:
+    print('The payment is pending.')
