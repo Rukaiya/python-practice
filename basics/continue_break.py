f078bde3efffaf6023d3ce00d11e33fbc915cab5
Rukaiya <rukaiyaamin36@gmail.com>
2022-12-12 15:03:07 +0600

    continue

---
+++ basics/continue_break.py
@@ -0,0 +1,16 @@
+for index in range(10):
+    if index % 2:
+        continue
+
+    print(index)
+
+print('------------')
+
+counter = 0
+while counter < 10:
+    counter += 1
+
+    if not counter % 2:
+        continue
+
+    print(counter)
